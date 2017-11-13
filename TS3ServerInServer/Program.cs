b5abd9c379e47688484d27a2f6398f133bbe2d0a
Bluscream <admin@timo.de.vc>
2017-11-13 15:32:21 +0100

    comit

--- TS3ServerInServer/Program.cs
+++ TS3ServerInServer/Program.cs
@@ -15,7 +15,16 @@ namespace TS3ServerInServer {
 		static int cnt = -1;
 		static string[] channels;
 		static string[] ids;
+		static string owner;
 		static ConnectionDataFull con = new ConnectionDataFull();
+		private static Random random = new Random();
+		private static string idfile = "ids.txt";
+		private static string chanfile = "chans.txt";
+		public static string RandomString(int length) {
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz0123456789";
+			return new string(Enumerable.Repeat(chars, length)
+			  .Select(s => s[random.Next(s.Length)]).ToArray());
+		}
 		static void Main() {
 			clients = new List<Ts3FullClient>();
 			Console.CancelKeyPress += (s, e) => {
@@ -28,38 +37,43 @@ namespace TS3ServerInServer {
 					Environment.Exit(0);
 				}
 			};
-			channels = File.ReadAllLines("chans.txt");
+			channels = File.ReadAllLines(chanfile);
 			var _con = channels[0].Split(',');
 			Console.WriteLine(channels[0]);
 			channels = channels.Skip(1).ToArray();
 			con.Address = _con[0];
 			con.Username = _con[1];
 			con.Password = _con[2];
+			owner = _con[4];
 			//con.VersionSign = VersionSign.VER_WIN_3_1_7_ALPHA;
-			if (!File.Exists("ids.txt")) {
-				using (File.Create("ids.txt")) { }
+			if (!File.Exists(idfile)) {
+				using (File.Create(idfile)) { }
 			}
-			ids = File.ReadAllLines("ids.txt");
+			ids = File.ReadAllLines(idfile);
 			for (int i = 0; i < channels.Length; i++) {
 				var client = new Ts3FullClient(EventDispatchType.DoubleThread);
-				client.OnConnected += Client_OnConnected;
-				client.OnDisconnected += Client_OnDisconnected;
-				client.OnErrorEvent += Client_OnErrorEvent;
+				client.OnConnected += OnConnected;
+				client.OnDisconnected += OnDisconnected;
+				client.OnErrorEvent += OnErrorEvent;
+				client.OnTextMessageReceived += OnTextMessageReceived;
 				//client.OnClientMoved += Client_OnClientMoved;
 				var _identity = ids.Select(x => x.Split(',')).ToList();
 				IdentityData ID;
 				try {
 					ID = Ts3Crypt.LoadIdentity(_identity[i][0], ulong.Parse(_identity[i][1]));
 				} catch (Exception) {
-					ID = Ts3Crypt.GenerateNewIdentity(26);
-					File.AppendAllText("ids.txt", ID.PrivateKeyString + "," + ID.ValidKeyOffset + "\r\n");
+					ID = Ts3Crypt.GenerateNewIdentity(Convert.ToInt32(_con[3]));
+					File.AppendAllText(idfile, ID.PrivateKeyString + "," + ID.ValidKeyOffset + "\r\n");
 				}
+				Console.WriteLine("#" + i + " UID: " +ID.ClientUid);
 				con.Identity = ID;
 				//Array values = Enum.GetValues(typeof(VersionSign));
 				//Random random = new Random();
 				//con.VersionSign = (VersionSign)values.GetValue(random.Next(values.Length));
 				//var t = typeof(VersionSign).GetFields();
 				con.VersionSign = VersionSign.VER_WIN_3_UNKNOWN;
+				con.HWID = RandomString(40);
+				Console.WriteLine("#" + i + " HWID: " + con.HWID);
 				client.Connect(con);
 				clients.Add(client);;
 				Thread.Sleep(2500);
@@ -68,19 +82,49 @@ namespace TS3ServerInServer {
 			Console.ReadLine();
 		}
 
-		/*private static void Client_OnClientMoved(object sender, IEnumerable<ClientMoved> e) {
-            foreach (var client in e) {
-                if (clie)
-            }
-        }*/
+		private static void OnTextMessageReceived(object sender, IEnumerable<TextMessage> msgs) {
+			foreach (var msg in msgs) {
+				if (msg.InvokerUid != owner) { continue; }
+				var _msg = msg.Message.ToLower();
+				try {
+					if (_msg.StartsWith("!scg ")) {
+						var client = (Ts3FullClient)sender;
+						var param = _msg.Replace("!scg ", "");
+						var _params = param.Split(',');
+						client.Send("setclientchannelgroup",
+							new CommandParameter("cgid", _params[0]),
+							new CommandParameter("cid", _params[1]),
+							new CommandParameter("cldbid", _params[2])
+						);
+					}
+					if (_msg.StartsWith("!badges ")) {
+						var client = (Ts3FullClient)sender;
+						var param = _msg.Replace("!badges ", "");
+						client.Send("clientupdate",
+							new CommandParameter("client_badges", param)
+						);
+					}
+				} catch (Ts3CommandException e) {
+					Console.WriteLine("Catched exception: " + e.Message);
+					continue;
+				}
+			}
+		}
 
-		private static void Client_OnDisconnected(object sender, DisconnectEventArgs e) {
+		private static void OnClientMoved(object sender, IEnumerable<ClientMoved> e) {
+            foreach (var client in e) {
+				if (client.InvokerUid == owner)
+					Console.WriteLine(owner + "joined some channel.");
+            }
+        }
+
+		private static void OnDisconnected(object sender, DisconnectEventArgs e) {
 			int myId = System.Threading.Interlocked.Increment(ref cnt);
 			var client = (Ts3FullClient)sender;
 			Console.WriteLine("Disconnected id={0} clid={1}", myId, client.ClientId);
 		}
 
-		private static void Client_OnConnected(object sender, EventArgs e) {
+		private static void OnConnected(object sender, EventArgs e) {
 			int myId = System.Threading.Interlocked.Increment(ref cnt);
 			var client = (Ts3FullClient)sender;
 			Console.WriteLine("Connected id={0} clid={1}", myId, client.ClientId);
@@ -128,6 +172,7 @@ namespace TS3ServerInServer {
 				}
 				Console.Write("\r\n");
 			}
+			return;
 			client.Send("setclientchannelgroup",
 				new CommandParameter("cgid", 11),
 				new CommandParameter("cid", cid),
@@ -136,12 +181,13 @@ namespace TS3ServerInServer {
 			//client.ClientMove(response., Ts3Crypt.HashPassword(channel[1]));
 		}
 	
-        private static void Client_OnErrorEvent(object sender, CommandError e) {
+        private static void OnErrorEvent(object sender, CommandError e) {
             var client = (Ts3FullClient)sender;
             Console.WriteLine(e.ErrorFormat());
             if (!client.Connected)
             {
-            	client.Connect(con);
+				Console.WriteLine("Could not connect: " + e.Message + " (" + e.ExtraMessage);
+            	//client.Connect(con);
             }
         }
     }
